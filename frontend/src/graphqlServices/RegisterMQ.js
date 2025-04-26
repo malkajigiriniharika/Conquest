9650649ddfffc475eb3c29aad9995a769bf8c1b0
malkajigiriniharika <121678468+malkajigiriniharika@users.noreply.github.com>
2025-04-26 20:44:50 +0530

    Update RegisterMQ.js

--- frontend/src/graphqlServices/RegisterMQ.js
+++ frontend/src/graphqlServices/RegisterMQ.js
@@ -2,7 +2,7 @@ import {gql} from "@apollo/client";
 
 export const REGISTER_MUTATION=gql`
     mutation Register($username: String!, $email: String!, $password: String!) {
-    register(username: $username, email: $email, password: $password){
+    register(name: $username,username: $username, email: $email, password: $password){
         isRegistered
     }
 }
